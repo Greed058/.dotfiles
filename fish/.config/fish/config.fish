78fb3f175f7b5e8dec4a64d03d0062ba282540da
greed-d <deekshit.bti@gmail.com>
2024-11-11 21:23:43 +0545

    `chore(add)` : add and remove some stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,6 +21,9 @@ bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
 #--------------------------------------> End of bang bang <--------------------------------------
 
+function postexec_test --on-event fish_postexec
+   echo
+end
 
 #--------------------------------------> Greeting <--------------------------------------
 
@@ -43,8 +46,12 @@ function chad
     env NVIM_APPNAME=nvchad nvim
 end
 
+function kvim
+  env NVIM_APPNAME=nvim.bak nvim
+end
+
  function nvims
-     set items astronvim nvchad lvim
+     set items astronvim nvchad lvim kvim
      set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
      if [ -z $config ]
          echo "Nothing selected"
@@ -67,8 +74,9 @@ alias lf "yazi"
 
 #--------------------------------------> userful alias <--------------------------------------
 
-alias lvim "env NVIM_APPNAME=LazyNvim nvim"
+alias lvim "env NVIM_APPNAME=lazyvim nvim"
 alias tvim "env NVIM_APPNAME=testvim nvim"
+alias kvim "env NVIM_APPNAME=nvim-bak nvim"
 alias .. "cd .."
 alias ... "cd ../.."
 alias .... "cd ../../.."
@@ -113,7 +121,9 @@ bind \cf "tmux-sessionizer"
 #--------------------------------------> PATH <--------------------------------------
 
 set PATH "$PATH":"$HOME/.local/scripts/"
+set PATH "$PATH":"$HOME/ghostty/zig-out/bin/"
 set PATH "$PATH":"$HOME/.local/bin/"
+set PATH "$PATH":"$HOME/.sdkman/candidates/gradle/current/bin/"
 set PATH "$PATH":"/usr/local/bin"
 set PATH "$PATH":"$HOME/.cargo/bin/"
 set -gx BROWSER floorp
@@ -122,6 +132,7 @@ fish_add_path -g -p $HOME/flutter/bin
 
 set --export ANDROID_HOME ~/Android/Sdk/
 # set PATH "$PATH":"$HOME/.fluttercont/flutter/bin"
+set PATH "$PATH":"/opt/android-sdk/platform-tools/"
 set PATH "$PATH":"$HOME/Android/Sdk/platform-tools/"
 set PATH "$PATH":"$ANDROID_HOME/tools/"
 set -gx ANDROID_SDK_ROOT ~/Android/Sdk/
@@ -152,63 +163,6 @@ zoxide init fish | source
 #--------------------------------------> spicetify <--------------------------------------
 
 fish_add_path /home/greed/.spicetify
-
-
-
-### SPARK ###
-# set -g spark_version 1.0.0
-#
-# complete -xc spark -n __fish_use_subcommand -a --help -d "Show usage help"
-# complete -xc spark -n __fish_use_subcommand -a --version -d "$spark_version"
-# complete -xc spark -n __fish_use_subcommand -a --min -d "Minimum range value"
-# complete -xc spark -n __fish_use_subcommand -a --max -d "Maximum range value"
-#
-# function spark -d "sparkline generator"
-#     if isatty
-#         switch "$argv"
-#             case {,-}-v{ersion,}
-#                 echo "spark version $spark_version"
-#             case {,-}-h{elp,}
-#                 echo "usage: spark [--min=<n> --max=<n>] <numbers...>  Draw sparklines"
-#                 echo "ezamples:"
-#                 echo "       spark 1 2 3 4"
-#                 echo "       seq 100 | sort -R | spark"
-#                 echo "       awk \\\$0=length spark.fish | spark"
-#             case \*
-#                 echo $argv | spark $argv
-#         end
-#         return
-#     end
-#
-#     command awk -v FS="[[:space:],]*" -v argv="$argv" '
-#         BEGIN {
-#             min = match(argv, /--min=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
-#             max = match(argv, /--max=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
-#         }
-#         {
-#             for (i = j = 1; i <= NF; i++) {
-#                 if ($i ~ /^--/) continue
-#                 if ($i !~ /^-?[0-9]/) data[count + j++] = ""
-#                 else {
-#                     v = data[count + j++] = int($i)
-#                     if (max == "" && min == "") max = min = v
-#                     if (max < v) max = v
-#                     if (min > v ) min = v
-#                 }
-#             }
-#             count += j - 1
-#         }
-#         END {
-#             n = split(min == max && max ? "▅ ▅" : "▁ ▂ ▃ ▄ ▅ ▆ ▇ █", blocks, " ")
-#             scale = (scale = int(256 * (max - min) / (n - 1))) ? scale : 1
-#             for (i = 1; i <= count; i++)
-#                 out = out (data[i] == "" ? " " : blocks[idx = int(256 * (data[i] - min) / scale) + 1])
-#             print out
-#         }
-#     '
-# end
-### END OF SPARK ###
-
 
 #THIS MUST BE AT THE END OF THE FILE FOR platform-tools TO WORK!!!
 set PATH "$PATH":"$HOME/Android_Stuff/SDK/platform-tools"
